3d54ad290d402cd227a02a3a204b34d5df48433c
elger <elgervb@gmail.com>
2014-10-07 23:35:18 +0200

    Eventlistening on the Player object

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -4,65 +4,133 @@
  * AudioContext: https://developer.mozilla.org/en-US/docs/Web/API/AudioContext
  * Web Audio lab: http://chimera.labs.oreilly.com/books/1234000001552/ch02.html#s02_2
  * styling html5 slider: http://brennaobrien.com/blog/2014/05/style-input-type-range-in-every-browser.html
+ * implement event model on custom object: http://stackoverflow.com/questions/15308371/custom-events-model-without-using-dom-events-in-javascript
  */
  var songs = [
   {
-    url: 'static/music/QOTSA-Little_Sister.ogg',
-    name: 'Little Sister',
-    type: 'ogg',
-    artist: 'Queest of the Stone Age'
+    url: 'static/music/01 Mumford And Sons - Sigh No More.mp3'
+  },
+  {
+    url: 'static/music/02 Mumford And Sons - The Cave.mp3'
+  },
+  {
+    url: 'static/music/03 Mumford And Sons - Winter Winds.mp3'
+  },
+  {
+    url: 'static/music/04 Mumford And Sons - Roll Away Your Stone.mp3'
+  },
+  {
+    url: 'static/music/05 Mumford And Sons - White Blank Page.mp3'
+  },
+  {
+    url: 'static/music/06 Mumford And Sons - I Gave You All.mp3'
+  },
+  {
+    url: 'static/music/07 Mumford And Sons - Little Lion Man.mp3'
+  },
+  {
+    url: 'static/music/08 Mumford And Sons - Timshel.mp3'
+  },
+  {
+    url: 'static/music/09 Mumford And Sons - Thistle & Weeds.mp3'
+  },
+  {
+    url: 'static/music/10 Mumford And Sons - Awake My Soul.mp3'
+  },
+  {
+    url: 'static/music/11 Mumford And Sons - Dust Bowl Dance.mp3'
+  },
+  {
+    url: 'static/music/12 Mumford And Sons - After the Storm.mp3'
   },
   {
     url: 'static/music/explosion.ogg',
     name: 'Explosion',
     type: 'ogg',
     artist : null
+  },
+  {
+    url: 'static/music/QOTSA-Little_Sister.ogg',
+    name: 'Little Sister',
+    type: 'ogg',
+    artist: 'Queest of the Stone Age'
   }
  ];
+var callbacks = {
+    progress :  function(starttime, duration){
+      var progress = document.getElementById('progress');
+      progress.max = duration.toFixed(1);
+      progress.value=starttime.toFixed(1);
+      progressTimer = setInterval(function(){
+        progress.value = parseFloat(progress.value) + .1;
+      }, 100);
+    },
+    stop : function(e){
+      document.getElementById('play').classList.remove('pause');
+      clearInterval(progressTimer);
+      document.getElementById('progress').value = 0;
+    },
+    play : function(e){
+      clearInterval(progressTimer);
+      callbacks.progress(e.detail.elapsed, e.detail.duration);
+    },
+    loading : function(e){
 
-var progressCallback = function(starttime, duration){
-  var progress = document.getElementById('progress');
-  progress.max = duration;
-  progress.value=starttime;
-  progressTimer = setInterval(function(){
-    progress.value = parseFloat(progress.value) + .1;
-  }, 100);
+    },
+    next : function(e){
+      clearInterval(progressTimer);
+      console.log('Next song');
+      e.detail.player.play();
+    },
+    pause : function(e){
+      clearInterval(progressTimer);
+      console.log('Pausing at ',e.detail.elapsed,'of',e.detail.duration );
+    },
+    end : function(e){
+      var el = document.getElementById('progress');
+      el.value = el.max;
+      console.log('finish playing');
+      callbacks.stop();
+      e.detail.player.next();
+      e.detail.player.play();
+    }
+ }
+ ,progressTimer;
 
-}, progressTimer;
 
 document.addEventListener('DOMContentLoaded', function () {
 
   var player = new Player(songs);
+  player.addEventListener('stop', callbacks.stop, false);
+  player.addEventListener('play', callbacks.play, false);
+  player.addEventListener('pause', callbacks.pause, false);
+  player.addEventListener('end', callbacks.end, false);
+  player.addEventListener('next', callbacks.next, false);
+  player.addEventListener('previous', callbacks.previous, false);
 
   /**
    * EVENT LISTENERS 
    */
   document.getElementById('play').addEventListener('mousedown', function(){
-    clearInterval(progressTimer);
-    this.classList.toggle('pause') ?  player.play(progressCallback) : player.pause();
+    this.classList.toggle('pause') ?  player.play() : player.pause();
   }, false);
 
   document.getElementById('stop').addEventListener('mousedown', function(){
-    document.getElementById('play').classList.remove('pause');
     player.stop();
-    clearInterval(progressTimer);
   });
-
   document.getElementById('next').addEventListener('mousedown', function(){
-    clearInterval(progressTimer);
     player.next();
   }, false);
 
   document.getElementById('previous').addEventListener('mousedown', function(){
-    clearInterval(progressTimer);
     player.previous();
   }, false);
 
-  document.getElementById('volume').addEventListener('input', function(){
-    document.getElementById('volumeDisplay').value = this.value;
-    player.volume(parseInt(this.value));
+  document.getElementById('gain').addEventListener('input', function(){
+    document.getElementById('gainDisplay').value = this.value;
+    player.gain(parseInt(this.value));
   }, false);
-  document.getElementById('volume').dispatchEvent(new Event('input'))
+  document.getElementById('gain').dispatchEvent(new Event('input'))
 });
 
 
@@ -76,30 +144,50 @@ var Player = function(songs){
     startTime = 0,                  // internal start time for calculating the elapsedTime
     elapsedTime = 0,                // elapsed time playing (for pause / resume)
     gainNode = context.createGain(),// The master volume
+  /**
+   * Event target for even handling
+   */
+  EventTarget= function() {
+    // Create a DOM EventTarget object
+    var target = document.createTextNode(null);
+    // Pass EventTarget interface calls to DOM EventTarget object
+    this.addEventListener = target.addEventListener.bind(target);
+    this.removeEventListener = target.removeEventListener.bind(target);
+    this.dispatchEvent = target.dispatchEvent.bind(target);
+  },
+  target = new EventTarget();
+  /**
+   * Create a custom audio player event
+   * @param string type the event type eg, stop, play, etc
+   * @param object data The event data to supply to the event 
+   */
+  createEvent = function(type, data){
+    return new CustomEvent(type, {detail: data||{}})
+  }
   clear = function(){
     source = null;
     elapsedTime = startTime = 0;
   },
   next = function(){
-    console.log('Next song');
     stop();
     clear();
     if (playlist.next()){
-      play();
+      target.dispatchEvent(createEvent('next', {player: this}) );
     }
   },
   pause = function(){
     source.stop(0);
     elapsedTime += context.currentTime - startTime;
-    console.log('Pausing at ',elapsedTime,'of',audioBuffer.duration );
+    target.dispatchEvent(createEvent('pause', {elapsed: elapsedTime, duration: audioBuffer.duration}));
   },
   /**
    * Play the current file
    * @param callback executed when playing with params starttime, duration
    */
-  play = function(callback) {
+  play = function() {
     // load new audio fragment
     if(!source){
+      target.dispatchEvent(createEvent('loading'));
       new Loader(playlist.current().url)
       .then(function(buffer, url){
         context.decodeAudioData(buffer, function(buffer) {
@@ -108,11 +196,22 @@ var Player = function(songs){
           source.buffer = audioBuffer;
           source.connect(gainNode);
           source.loop = false;
+          source.onended = function(e){
+            // this is also called when pausing
+            if (audioBuffer.duration <= elapsedTime){
+              if (source){source.stop(0);}
+              target.dispatchEvent( createEvent('end', {player: this}) );
+            }
+          }
           startTime = context.currentTime;
           source.start(0);
-          if (typeof callback ==='function'){
-            callback(0, audioBuffer.duration); 
-          }
+          
+          target.dispatchEvent(createEvent('play', {
+              'elapsed': 0, 
+              'duration': audioBuffer.duration
+            })
+          );
+
           console.log('Start playing');
         }, function(){
          console.log('Error encoding file ' + url);
@@ -124,11 +223,22 @@ var Player = function(songs){
       source.buffer = audioBuffer;
       source.connect(gainNode);
       source.loop = false;
+      source.onended = function(e){
+        // this is also called when pausing
+        if (audioBuffer.duration <= elapsedTime){
+          if (source){source.stop(0);}
+          target.dispatchEvent( createEvent('end', {player: this}) );
+        }
+      }
       startTime = context.currentTime;
       source.start(0, elapsedTime % audioBuffer.duration);
-      if (typeof callback ==='function'){
-        callback(elapsedTime % audioBuffer.duration, audioBuffer.duration); 
-      }
+      
+      target.dispatchEvent(createEvent('play', {
+            elapsed: elapsedTime % audioBuffer.duration, 
+            duration: audioBuffer.duration
+          })
+      );
+      
       console.log('Resuming at ',elapsedTime,'of',audioBuffer.duration);
     }
   },
@@ -137,7 +247,7 @@ var Player = function(songs){
     stop();
     clear();
     if (playlist.previous()){
-      play();
+      target.dispatchEvent(createEvent('previous', {player: this}));
     }
   },
   stop = function(){
@@ -146,26 +256,31 @@ var Player = function(songs){
     }
     elapsedTime = startTime = 0;
     console.log('Stopped');
+    target.dispatchEvent(createEvent('stop'));
   },
-  volume = function(gain){
+  gain = function(gain){
     gainNode.gain.value = gain === 0 ? gain.toFixed(2) : (gain / 100).toFixed(2);
-    console.log('setting volume to ',gain.toFixed(0));
+    target.dispatchEvent(createEvent('gain', {gain: gain.toFixed(2)}));
+    console.log('setting gain to ', gain.toFixed(0));
   };
 
   // fix prefixing
   window.AudioContext = window.AudioContext  || window.webkitAudioContext;
   if (!window.AudioContext){console.error("Web Audio API is not supported by this browser.");}
-
   gainNode.connect(context.destination);
+  target.dispatchEvent(createEvent('init'));
 
   return {
-    next : next,
-    pause : pause,
-    play : play,
+    addEventListener    : target.addEventListener,
+    dispatchEvent       : target.dispatchEvent,
+    removeEventListener : target.removeEventListener,
+    next     : next,
+    pause    : pause,
+    play     : play,
     playlist : function(){return playlist;},
     previous : previous,
-    stop : stop,
-    volume : volume
+    stop     : stop,
+    gain     : gain
   }
 };
 var Loader = function (url) {
