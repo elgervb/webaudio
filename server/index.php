b7bb449e169939b10a9bd0342c36774fe3901182
elger <elgervb@gmail.com>
2014-10-07 23:35:35 +0200

    fix imports

--- server/index.php
+++ server/index.php
@@ -3,6 +3,6 @@ use core\ClassLoader;
 
 include '../../framework/core/Classloader.php';
 $loader = ClassLoader::create();
-$loader->addClassPath('../framework');
+$loader->addClassPath('../../framework');
 $loader->addClassPath('app/lib');
 core\FrontController::start();
