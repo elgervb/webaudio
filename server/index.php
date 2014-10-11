70a8a7e638a824659eb05514d8edca2bc4c7ed88
elger <elgervb@gmail.com>
2014-10-11 15:13:09 +0200

    PHP imports

--- server/index.php
+++ server/index.php
@@ -1,8 +1,7 @@
 <?php
 use core\ClassLoader;
 
-include '../../../framework/core/Classloader.php';
+include '../../framework/core/Classloader.php';
 $loader = ClassLoader::create();
-$loader->addClassPath('../../../framework');
 $loader->addClassPath('app/lib');
 core\FrontController::start();
