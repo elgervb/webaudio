b34526b2e8dcc0ec964e5d8566336b5df2816cd8
elger <elgervb@gmail.com>
2014-11-12 23:39:00 +0100

    dropped support to expose UNC paths
    cannot scan MP3 files over UNC path and client cannot load them...
    dropped
    
    fixes #65

--- server/app/lib/audio/scanner/DirScanner.php
+++ server/app/lib/audio/scanner/DirScanner.php
@@ -1,27 +1,41 @@
 <?php
-
 namespace audio\scanner;
 
+use utils\CallbackFilterIterator;
+use core\filesystem\Filesystem;
+
 class DirScanner{
+	
+	private $preservePath;
+	
+	public function __construct($aPreservePath = false){
+		$this->preservePath = $aPreservePath;	
+	}
 	/**
 	 * Scans a directory for files with the given extensions
 	 * @param string|\SplFileInfo $aDir
 	 * @param array $aExtensions
 	 * @return \CallbackFilterIterator|boolean
 	 */
-	public static function scan($aDir, array $aExtensions){
+	public function scan($aDir, array $aExtensions){
 		$dirIterator = new \RecursiveDirectoryIterator( $aDir );
-		$dirIterator->setInfoClass('audio\scanner\SecureFileInfo');
+		if (!$this->preservePath){
+			$dirIterator->setInfoClass('audio\scanner\SecureFileInfo');
+		}
 		
-		return new \CallbackFilterIterator(
+		$preservePath = $this->preservePath;
+		
+		return new CallbackFilterIterator(
 			new \RecursiveIteratorIterator(
 				$dirIterator,\RecursiveIteratorIterator::CHILD_FIRST
 			),
-			function($current, $key, $iterator) use ($aDir, $aExtensions){
-				/* @var $current \audio\scanner\SecureFileInfo */
-				$current->setRelativePath($aDir);
+			function($current, $key, $iterator) use ($aDir, $aExtensions, $preservePath){
+				if (!$preservePath){
+					/* @var $current \audio\scanner\SecureFileInfo */
+					$current->setRelativePath($aDir);
+				}
 
-				return $current->isFile() &&  in_array($current->getExtension(), $aExtensions);
+				return $current->isFile() &&  in_array(Filesystem::getExtension( $current ), $aExtensions);
 			}
 		);
 	}
